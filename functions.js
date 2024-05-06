8494a9ae6b35c61ec016acda4a9f196db3d11380
Jaranesca Braker <142855048+jaranesca@users.noreply.github.com>
2024-05-06 10:33:52 -0700

    function.html code
    updated code

---
+++ functions.js
@@ -0,0 +1,16 @@
+"use strict";
+function generateRandomName(){
+}
+
+function SayMyName(mysecretcode) {
+    let myname = document.getElementById("myName").value;
+    console.log(myname);
+    alert(`Hi ${myname} - ${mysecretcode}`);
+  }
+
+  function add(number1, number2){
+    return number1 + number2;
+  }
+  console.log(add(2,4));
+
+  //this page is for practice
